fe452518c89225e4ddb0a21e5e1b9bcca4b5064f
charlessir <dedcats.lol@gmail.com>
2026-03-24 17:18:08 -1000

    Add AutoRebirth configuration and load script

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
-a
+-- CONFIG
+_G.AutoRebirthConfig = {
+    AutoStart = true,        -- start automatically
+    QueueOnTeleport = true,  -- auto run after rebirth teleport
+    CountRebirths = true,    -- track rebirth count
+    TeleportDelay = 1        -- delay before teleport
+}
+
+-- LOAD SCRIPT
+loadstring(game:HttpGet("https://github.com/charlessir/RAF2AutoRebirth/raw/refs/heads/main/source.lua"))()
